48e904f5ac00110c2dee10936516b9d686431794
OnyeEbube <okontaonyeka@gmail.com>
2023-07-24 20:56:37 +0000

    adding solution file 3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+            """Return hyper_index
+            """
+            assert index is None or isinstance(index, int), "Index must be an integer."
+            assert page_size > 0, "Page size must be a positive integer."
+
+            indexed_dataset = self.indexed_dataset()
+            max_index = len(indexed_dataset) - 1
+
+            if index is None:
+                index = 0
+            else:
+                assert 0 <= index <= max_index, "Invalid index."
+
+            next_index = index + page_size
+            if next_index > max_index:
+                next_index = None
+
+            data = []
+            for i in range(index, min(index + page_size, max_index + 1)):
+                if i in indexed_dataset:
+                    data.append(indexed_dataset[i])
+
+            hyper_metadata = {
+                    "index": index,
+                    "next_index": next_index,
+                    "page_size": page_size,
+                    "data": data
+                }
+            return hyper_metadata
