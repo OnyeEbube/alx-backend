ff2d4c40777c56ee9172a3366c2b1c58785fdeae
OnyeEbube <okontaonyeka@gmail.com>
2023-07-24 15:07:41 +0000

    adding file 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Write a function named index_range that takes two integer
+arguments page and page_size."""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """Returns a tuple(start_ind, end_ind)"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns dataset list
+        """
+        assert isinstance(
+                page, int), "raised when page and/or page_size are not ints"
+        assert page != 0, "raised with 0"
+        assert page > 0, "raised with negative values"
+        assert type(
+                page_size) is int, "raised when page and/or page_size are not ints"
+
+        assert page_size != 0, "raised with 0"
+        assert page_size > 0, "raised with negative values"
+        dataset = self.dataset()
+
+        start_index, end_index = index_range(page, page_size)
+        return dataset[start_index:end_index]
+
+        if start_index >= len(dataset) or start_index < 0:
+            return []
