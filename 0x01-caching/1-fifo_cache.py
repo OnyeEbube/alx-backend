50ea9c625946a72ad7d8cacfae372dff1b345d85
OnyeEbube <okontaonyeka@gmail.com>
2023-07-26 20:41:04 +0000

    Adding some files

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""1-fifo_cache"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        """_summary_"""
+        super().__init__()
+        self.keys_order = []
+
+    def put(self, key, item):
+        """_summary_"""
+        if key is not None or item is not None:
+            j = 0
+            for i in self.cache_data:
+                j += 1
+            if j >= BaseCaching.MAX_ITEMS:
+                first_key = self.keys_order[0]
+                self.keys_order.pop(0)
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
+            self.cache_data[key] = item
+            self.keys_order.append(key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
