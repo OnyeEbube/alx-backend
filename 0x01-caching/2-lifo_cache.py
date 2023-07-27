b2bcf2ba849c807a4097a7b9cb4705621c5d25bc
OnyeEbube <okontaonyeka@gmail.com>
2023-07-27 09:53:08 +0000

    Adding solutions for task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""1-fifo_cache"""
+"""
+Create a class LIFOCache that inherits from BaseCaching and is a
+caching system"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """_summary_
     """
     def __init__(self):
@@ -17,22 +19,19 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """_summary_
         """
-        if key is not None or item is not None:
+        if key is None or item is None:
+            pass
+        else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data:
-                first_key = self.keys_order.pop(0)
-                del self.cache_data[first_key]
-                print("DISCARD: {}".format(first_key))
+                    and key not in self.cache_data.keys():
+                last_key = self.keys_order.pop(-1)
+                del self.cache_data[last_key]
+                print("DISCARD: {}".format(last_key))
             self.cache_data[key] = item
             self.keys_order.append(key)
-        else:
-            pass
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
+        """_summary_
         """
         if key is None or key not in self.cache_data.keys():
             return None
