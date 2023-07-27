9d1604f11df940aa2583d2e61cead7da2b1d6459
OnyeEbube <okontaonyeka@gmail.com>
2023-07-27 08:04:44 +0000

    Adding corrections to file 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """0-basic_cache"""
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -10,10 +12,12 @@ class BasicCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        """_summary_"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """_summary_"""
         if key is not None or key in self.cache_data.keys():
             return self.cache_data.get(key)
         return None
