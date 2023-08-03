45027960f7af2087b363dc88b0babd576cbe9584
OnyeEbube <okontaonyeka@gmail.com>
2023-08-03 18:23:38 +0000

    Making corrections to files 2 and 3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -30,6 +30,7 @@ def index():
     """
     return render_template('2-index.html')
 
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -31,6 +31,7 @@ def index():
     """
     return render_template('3-index.html')
 
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
